9069a38538aa88f65bb5a0f3c0f28ee0c20ba78e
Ibna Kowsar <kawser.ibn.93@gmail.com>
2024-04-14 13:12:35 -0500

    20 Valid Paranthesis

--- Easy/valid_parantheses.py
+++ Easy/valid_parantheses.py
@@ -1,39 +1,16 @@
-
-def isValid(s):
-    """
-    :type s: str
-    :rtype: bool
-    """
-    checker = []
-    last = None
-    sample = ['(', '[','{']
-    pop_sample = {')': '(', ']' : '[', '}' : '{'}
-
-    if len(s) < 1 or len(s)%2 != 0 or s[0]in pop_sample:
-        return False
-    else:
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        checker = []
+        pop_sample = {'(': ')', '[' : ']', '{' : '}'}
         
-        for x,i in enumerate(s):
-            print(x, i, len(s)-1, len(checker), last)
-            print(checker)
-            print( x == len(s)-1, len(checker) == 0)
-
-
-            if i in sample:
-                last = i
-                checker.append(i)
-
-            elif i in pop_sample.keys():
-                if len(checker) != 0 and (pop_sample[i] == last or checker[-1] == pop_sample[i]):
-                  # print(checker.pop(), end = ' mmmm.  ')
-                  # print(len(checker))
-                  checker.pop()
-                else:
-                  return False
-            if x == len(s)-1 and len(checker) == 0:
-              return True
-        
-            
-
-        return False
-isValid("[(({})}]")
+        for b in range(len(s)):
+            if s[b] in pop_sample.keys():
+              checker.append(s[b])
+            elif len(checker) == 0 or s[b] != pop_sample[checker.pop()]:
+              return False
+        if len(checker) == 0:
+          return True
